8cd2d968fd936a19e42f1a857f2bac824eb56d7f
praveen <pravens97@gmail.com>
2019-02-23 12:46:50 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,6 @@
+def display():
+    print(__name__)
+
+def Addition(x,y):
+    z = x + y;
+    return z
---
+++ calc.py
@@ -0,0 +1,6 @@
+import add
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+# res = add.Addition(num1,num2)
+print (__name__)
+add.display()
